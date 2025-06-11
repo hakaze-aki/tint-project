19bf8641f5bfbeb82351eefcb00fe4e151c823ce
KangYoonJu <mikaze51244@gmail.com>
2025-06-11 10:20:13 +0900

    basic setting1

--- src/main/java/com/example/tint/DummyDataLoader.java
+++ src/main/java/com/example/tint/DummyDataLoader.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 import java.util.List;
 
-//@Component
+@Component
 @AllArgsConstructor
 public class DummyDataLoader implements CommandLineRunner {
 
